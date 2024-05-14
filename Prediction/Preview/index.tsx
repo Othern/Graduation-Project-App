8fb4e50883fc24a927f9ea8baf832d47b440140a
Othern <willy910928@gmail.com>
2024-05-14 17:56:28 +0800

    The code utilizes the map function to loop through an array of prediction data and efficiently render corresponding PredictionCard components. This approach condenses repetitive code and enhances readability.
    Initially, an array named predictionData is defined to store prediction card data. The map function iterates over this array, dynamically creating PredictionCard components for each data item. Properties such as name, percentage, and event handlers are assigned based on the corresponding data. Finally, the renderPredictionCards function is invoked to render all prediction cards efficiently in the component. Overall, these modifications streamline the rendering process, resulting in cleaner and more maintainable code.

--- Prediction/Preview/index.tsx
+++ Prediction/Preview/index.tsx
@@ -1,18 +1,30 @@
 import PredictionCard from "./PredictionCard"
 import { ScrollView } from "react-native"
 
-export default (props:any) => {
-    const handlePress = (screenName: string,params:any) => {
-        props.navigation.push(screenName,params);
-      };
+const predictionData = [
+    { name: "隧道口", percentage: 20 },
+    { name: "理工二道", percentage: 40 },
+    { name: "管院停車場", percentage: 60 },
+    { name: "國研大樓", percentage: 80 }
+];
+export default (props: any) => {
+    const handlePress = (screenName: string, params: any) => {
+        props.navigation.push(screenName, params);
+    };
     return (
-            <>  
-                <ScrollView>
-                    <PredictionCard  name ={"隧道口"} percentage={20} props={props} onPress={() => handlePress("Details",{title:"隧道口",percentage : 20})} />
-                    <PredictionCard  name ={"理工二道"} percentage={40} props={props} onPress={() => handlePress("Details",{title:"理工二道",percentage : 40})} />
-                    <PredictionCard  name ={"管院停車場"} percentage={60} props={props} onPress={() => handlePress("Details",{title:"管院停車場",percentage : 60})} />
-                    <PredictionCard  name ={"國研大樓"} percentage={60} props={props} onPress={() => handlePress("Details",{title:"國研大樓",percentage : 60})} />
-                </ScrollView>
-            </>
+        <>
+            <ScrollView>
+                {predictionData.map((data, index) => (
+                    <PredictionCard
+                        key={index}
+                        name={data.name}
+                        percentage={data.percentage}
+                        props={props}
+                        onPress={() => handlePress("Details", { title: data.name, percentage: data.percentage })}
+                    />
+                ))}
+
+            </ScrollView>
+        </>
     )
 }
