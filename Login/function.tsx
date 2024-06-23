da0ef03edacff6d88130d70ab4039c40047108a2
5241513 <96127309+5241513@users.noreply.github.com>
2024-06-24 03:51:58 +0800

    Finished the contact with backend and save userdatas to local device and fast login
    contact with backend:
    briefly finished, only check it will request, but haven't check server yet.
    Save userdata on device:
    finished (some use keychain and some use asyncstorage)
    FastLogin:
    similiar to contact wih backend part, only check it will request to server, haven't actually check the server response, use the data save by keychain to fast login
    
    import two more packages:
    asycnostorage &
    keychain
    recoded in README

---
+++ Login/function.tsx
@@ -0,0 +1,107 @@
+import { Alert } from "react-native";
+import Toast from 'react-native-toast-message';
+import * as Keychain from 'react-native-keychain';
+//keychain 如果要在ios 用 要在 ios資料夾中運行 npx pod-install 
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+export const showToast = (text1: string, text2: string, type = 'success') => {
+    Toast.show({
+        type: type,
+        text1: text1,
+        text2: text2,
+        topOffset: 65
+    });
+}
+
+export async function saveCredentials(email: string, password: string) {
+    try {
+        // 儲存Email和密碼
+        await Keychain.setGenericPassword(email, password);
+        console.log('Credentials saved successfully!');
+    } catch (error) {
+        console.error('Could not save credentials', error);
+    }
+}
+
+export async function getCredentialsFromKeychain() {
+    try {
+        const credentials = await Keychain.getGenericPassword();
+        if (credentials) {
+            return { email: credentials.username, password: credentials.password };
+        } else {
+            console.log('No credentials stored');
+            return null;
+        }
+    } catch (error) {
+        console.error('Could not load credentials', error);
+        return null;
+    }
+}
+
+export const saveData = (key: any, value: any) => {
+    try {
+        AsyncStorage.setItem(key, value);
+    } catch (e) {
+        console.log("error", e);
+    }
+};
+
+export const getDataJSON = async (key: any, success = (data: any) => { }) => {
+    try {
+        const value = await AsyncStorage.getItem(key);
+        if (value) {
+            const valueParse = JSON.parse(value);
+            success(valueParse);
+        }
+    } catch (e) {
+        console.log("error", e);
+    }
+};
+
+export async function submitLogin(email: string, password: string, success = (data: any) => { }, fail = (data: any) => { }) {
+    try {
+        const response = await fetch('http://172.20.10.2:4000/loginSubmit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email, password })
+        });
+        const responseData = await response.json();
+        if (responseData.state === "success") {
+            success({
+                headImg: responseData.headImg,
+                username: responseData.username
+            });
+        } else {
+            fail(responseData.state); // Pass state as error
+            Alert.alert("登入失敗.");
+        }
+    } catch (error) {
+        console.error('Error sending login data:', error);
+    }
+}
+
+export async function submitRegister(email: string, password: string, username: string, success = (data: any) => { }, fail = (data: any) => { }) {
+    try {
+        const response = await fetch('http://172.20.10.2:4000/registerSubmit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email, password, username })
+        });
+        const responseData = await response.json();
+        if (responseData.state === "success") {
+            success({
+                headImg: responseData.headImg,
+            });
+        } else {
+            fail(responseData.state); // Pass state as error
+            Alert.alert("註冊失敗.");
+        }
+    } catch (error) {
+        console.error('Error sending Register data:', error);
+    }
+}
