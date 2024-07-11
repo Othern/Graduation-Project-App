b5e06e17007b3ca27cf0e8d4caa505066397fd00
orangeyuzi <eva134679102@gmail.com>
2024-07-11 18:05:16 +0800

    add flask v2
    use Blueprint to concat all flask file, add modify file, head images modifying will be complete later
    add email to filter who is the user in DB

---
+++ flask_server/modify.py
@@ -0,0 +1,159 @@
+from flask import Flask, render_template, jsonify, request, url_for, redirect, session, Blueprint
+from flask_cors import CORS
+import secrets
+import mariadb
+import sys
+
+
+# 建立實體
+modify = Blueprint('modify', __name__, template_folder='C:\\Users\\eva13\\AwesomeProject\\Graduation-Project-App')
+CORS(modify) # 跨平台使用
+
+modify.secret_key = secrets.token_hex(16) # 保護session
+
+@modify.route('/')
+def index():
+    return render_template("Profile\\Modify\\index.tsx")
+
+@modify.route('/ModifyPassword', methods=["GET", "POST"])
+def change_password():
+    if request.method == "POST":
+        # 取表單資料
+        data = request.get_json()
+        email = data.get('currentEmail')
+        old_pw = data.get('newPassword')
+        new_pw = data.get('oldPassword')
+        
+        # 連接資料庫
+        try:
+            conn = mariadb.connect(
+                user="root", 
+                password="mis114monkey", 
+                host="127.0.0.1", 
+                port=3307, 
+                database="mis114_monkey" 
+            )
+        except mariadb.Error as e:
+            print(f"Error connecting to MariaDB Platform: {e}")
+            sys.exit(1)
+        
+        # 讀取資料庫資料
+        cur = conn.cursor()
+        cur.execute("SELECT Password FROM user WHERE Email=? LIMIT 1", (email,))
+        rows = cur.fetchall()
+        if rows:
+            for row in rows:
+                if old_pw == str(row[0]) and old_pw != new_pw and new_pw.isalnum():
+                    try: 
+                        cur.execute("UPDATE user SET Password=? WHERE Email=?", (new_pw, email))
+                        conn.commit() 
+                    except mariadb.Error as e: 
+                        print(f"Error: {e}")
+                    session['state'] = "success"
+                else:
+                    session['state'] = "wrong"
+
+        else:
+            session['state'] = "wrong"
+        conn.close()
+
+        return redirect(url_for("modify.change_password")) # 網頁重新導向，避免重複提交資料
+    # 讀取session資料
+    state = session.get('state', "wrong")
+    session.clear() # 將session裡的資料清除
+    
+    return jsonify({"state": state})
+
+@modify.route('/ModifyUsername', methods=["GET", "POST"])
+def change_username():
+    if request.method == "POST":
+        # 取表單資料
+        data = request.get_json()
+        email = data.get('currentEmail')
+        new_name = data.get('newUsername')
+        
+        # 連接資料庫
+        try:
+            conn = mariadb.connect(
+                user="root", 
+                password="mis114monkey", 
+                host="127.0.0.1", 
+                port=3307, 
+                database="mis114_monkey" 
+            )
+        except mariadb.Error as e:
+            print(f"Error connecting to MariaDB Platform: {e}")
+            sys.exit(1)
+        
+        # 讀取資料庫資料
+        cur = conn.cursor()
+        cur.execute("SELECT User_name FROM user WHERE Email=? LIMIT 1", (email,))
+        rows = cur.fetchall()
+        if rows:
+            cur.execute("SELECT User_name FROM user WHERE User_name=? LIMIT 1", (new_name,))
+            rows = cur.fetchall()
+            if rows:
+                session['state'] = "wrong"
+            else:
+                cur.execute("UPDATE user SET User_name=? WHERE Email=?", (new_name, email))
+                conn.commit()
+                session['state'] = "success"
+        else:
+            session['state'] = "wrong"
+        conn.close()
+
+        return redirect(url_for("modify.change_username")) # 網頁重新導向，避免重複提交資料
+    # 讀取session資料
+    state = session.get('state', "wrong")
+    session.clear() # 將session裡的資料清除
+    
+    return jsonify({"state": state})
+
+# @modify.route('/ModifyModifyHeadImg', methods=["GET", "POST"])
+# def change_headimg():
+#     if request.method == "POST":
+#         # 取表單資料
+#         data = request.get_json()
+#         email = data.get('currentEmail')
+#         new_name = data.get('newUsername')
+        
+#         # 連接資料庫
+#         try:
+#             conn = mariadb.connect(
+#                 user="root", 
+#                 password="mis114monkey", 
+#                 host="127.0.0.1", 
+#                 port=3307, 
+#                 database="mis114_monkey" 
+#             )
+#         except mariadb.Error as e:
+#             print(f"Error connecting to MariaDB Platform: {e}")
+#             sys.exit(1)
+        
+#         # 讀取資料庫資料
+#         cur = conn.cursor()
+#         cur.execute("SELECT User_name FROM user WHERE Email=? LIMIT 1", (email,))
+#         rows = cur.fetchall()
+#         if rows:
+#             cur.execute("SELECT User_name FROM user WHERE User_name=? LIMIT 1", (new_name,))
+#             rows = cur.fetchall()
+#             if rows:
+#                 session['state'] = "wrong"
+#             else:
+#                 cur.execute("UPDATE user SET User_name=? WHERE Email=?", (new_name, email))
+#                 conn.commit()
+#                 session['state'] = "success"
+#         else:
+#             session['state'] = "wrong"
+#         conn.close()
+
+#         return redirect(url_for("modify.change_headimg")) # 網頁重新導向，避免重複提交資料
+#     # 讀取session資料
+#     state = session.get('state', "wrong")
+#     session.clear() # 將session裡的資料清除
+    
+#     return jsonify({"state": state})
+
+# 執行程式
+if __name__ == '__main__':
+    modify.run(host="0.0.0.0", port=5000) # 我先設這個，有更好的話可以直接提出來討論修改
